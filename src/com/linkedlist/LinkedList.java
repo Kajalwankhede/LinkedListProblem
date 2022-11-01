b9b3c84341cc62141c0a353b3f80255ee69f2771
Kajakwankhede <Kajalwankhede@example.com>
2022-11-01 16:29:21 +0530

    Create simple Linked list of 56,30 and 70

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -1,8 +1,44 @@
 package com.linkedlist;
 
 public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Linked List Program.");
-    }
 
+    class Node {   // Create a node
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+public Node head=null;
+    public Node tail=null;
+    public void addNode(int d){
+        Node newNode=new Node(d);
+        if (head==null){
+            head=newNode;
+            tail=newNode;
+        }
+        else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    public void display() {                 // Print the linked list
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+
+    }
+    public static void main(String[] args) {
+        LinkedList li = new LinkedList();
+        li.addNode(56);
+        li.addNode(30);
+        li.addNode( 70);
+        System.out.println("Linked list: ");
+        li.display();
+
+    }
 }
